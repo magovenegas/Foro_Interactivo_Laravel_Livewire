e2207c037d40d080b34f715a1fe4fa171932758a
camilo.venegas-at-298920916842 <camilo.venegas@assisprex.com>
2025-05-27 21:14:10 -0500

    Se suben las nuevas dependencias

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,31 @@
 <?php
 
-use Illuminate\Htto\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/**
-*  Route::get       CONSULTAR
-*  Route::post      GUARDAR
-*  Route::delete    ELIMINAR 
-*  Route::put       ACTUALIZAR 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return "hola";
+    return view('welcome');
 });
 
-Route::get('blog', function (){
-    $posts = [
-        ['id'=>1, 'titel'=>'PHP', 'slug'=>'php'],
-        ['id'=>2, 'titel'=>'Laravel', 'slug'=>'Laravel']
-    ];
-    return view('blog',['posts'=>$posts]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
